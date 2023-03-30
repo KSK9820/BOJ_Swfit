5fa21a4c7e00d559d554f18b739732a100a4e809
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-03-30 15:21:39 +0900

    문자열 27866

---
+++ 문자열/문자열/27866.swift
@@ -0,0 +1,14 @@
+//
+//  27866.swift
+//  문자열
+//
+//  Created by 김수경 on 2023/03/30.
+//
+
+import Foundation
+
+let line = Array(readLine()!)
+let number = Int(readLine()!)!
+
+print(line[number-1])
+
